662c94b83d6af1a4815f313995b373c6e51a006b
TheCountOfNicolo <nunurbergen@yandex.ru>
2025-01-20 18:09:59 +0500

    WelcomePage

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,21 +1,4 @@
-using Microsoft.AspNetCore.HttpLogging;
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddHttpLogging(opts =>
-    opts.LoggingFields = HttpLoggingFields.RequestProperties);
-
-builder.Logging.AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information);
-
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseHttpLogging();
-}
-
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/person", () => new Person("Andrew", "Jonhy"));
-
+app.UseWelcomePage();
 app.Run();
-
-public record Person(string FirstName, string SecondName);
