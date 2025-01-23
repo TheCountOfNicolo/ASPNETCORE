85a93ee65effc6323a02095fa00723634c5971cf
TheCountOfNicolo <nunurbergen@yandex.ru>
2025-01-23 12:06:13 +0500

    Объединение в конвейер!

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,4 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-app.UseWelcomePage();
+app.UseDeveloperExceptionPage();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapGet("/", () => "Hello World");
 app.Run();
