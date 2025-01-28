cb91caf0ef0301785ec9dc5dd64ea8e5a4a005dd
TheCountOfNicolo <nunurbergen@yandex.ru>
2025-01-28 11:25:57 +0500

    work with parameters

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,8 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-app.UseDeveloperExceptionPage();
-app.UseStaticFiles();
-app.UseRouting();
 
-app.MapGet("/", () => "Hello World");
+List<Person> list = new List<Person>{
+    new("Max","Alex"),
+    new("Bonny","Clide"),
+    new("Boris","Barbaris"),
+    new("Elena","Carl"),
+    new("Avalon","Asteroid"),
+    new("Arkhimed","Asterix")
+};
+
+app.MapGet("/person{name}", (string name) => list.Where(p => p.firstName.StartsWith(name)));
 app.Run();
+
+public record Person(string firstName, string secondName);
