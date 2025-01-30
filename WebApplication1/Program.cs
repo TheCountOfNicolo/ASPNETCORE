844a766f70f11dafed1581c0d322786fe4f92e2c
TheCountOfNicolo <nunurbergen@yandex.ru>
2025-01-30 11:43:43 +0500

    Work with HttpResponse!

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,33 +1,13 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-app.MapGet("/fruit", () => Fruit.All);
+using System.Net.Mime;
 
-var getFruit = (string id) => Fruit.All[id];
-app.MapGet("/fruit/{id}", getFruit);
-app.MapPost("/fruit/{id}", Handlers.AddFruit);
-Handlers handlers = new();
-app.MapPut("fruit/{id}", handlers.ReplaceFruit);
-app.MapDelete("/fruit/{id}", DeleteFruit);
+WebApplicationBuilder build = WebApplication.CreateBuilder(args);
+var app = build.Build();
+
+app.MapGet("/", (HttpResponse httpResponse) =>
+{
+    httpResponse.StatusCode = 418;
+    httpResponse.ContentType = MediaTypeNames.Text.Plain;
+    return httpResponse.WriteAsync("I am a teapot!");
+});
+
 app.Run();
-
-void DeleteFruit(string id)
-{
-    Fruit.All.Remove(id);
-}
-
-record Fruit(string Name, int Stock)
-{
-    public static readonly Dictionary<string, Fruit> All = new();
-}
-
-class Handlers
-{
-    public void ReplaceFruit(string id, Fruit fruit)
-    {
-        Fruit.All[id] = fruit;
-    }
-    public static void AddFruit(string id, Fruit fruit)
-    {
-        Fruit.All.Add(id, fruit);
-    }
-}
